79c447e5cd64711e676ce83573d490bfc4d64f78
lufamukr <lufamukr@gmail.com>
2024-04-26 20:58:15 +0200

    plus del todolist

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,72 @@
-import React, { ChangeEvent, useState } from 'react';
-import './App.css';
-import { AddTodo } from './comp-ts/inputTodo/AddTodo';
-
+import React, { ChangeEvent, useState } from "react";
+import "./App.css";
+import { AddTodo } from "./comp-ts/inputTodo/AddTodo";
+import { v4 as uuidv4 } from "uuid";
 
 export type TodolistStateType = {
   title: string;
   id: string;
-}
+};
 
-function App() {
+const App = () => {
+  let todoState: Array<TodolistStateType> = [];
+  let [todolistState, setTodolistState] = useState(todoState);
+  let [todolistTitle, setTodolistTitle] = useState("");
 
-  let todoState:Array<TodolistStateType> = []
-  let [todolistState, setTodolistState] = useState(todoState) 
-  let [todolistTitle, setTodolistTitle] = useState('')
+  let err = "";
 
-  let err = ''
-
-
-  function onClickHandler(title:string) {
-    if(title.trim() !== '') {
-      let newTodo = {id: '1', title: title}
-      let newTodoState = [...todolistState, newTodo]
-      setTodolistState(newTodoState)
-      setTodolistTitle('')
+  function onClickHandler(title: string) {
+    if (title.trim() !== "") {
+      let newTodo = { id: uuidv4(), title: title };
+      let newTodoState = [...todolistState, newTodo];
+      setTodolistState(newTodoState);
+      setTodolistTitle("");
     } else {
-      alert('enter a name')
+      alert("enter a name");
     }
-
   }
 
-  function addTodoTitle(titleName:string) {
-    setTodolistTitle(titleName)
+  function addTodoTitle(titleName: string) {
+    setTodolistTitle(titleName);
   }
 
   function deleteItem(id: string) {
-
+    let newTodolistState = todolistState.filter((f) => {
+      return f.id !== id;
+    });
+    setTodolistState(newTodolistState);
   }
 
-  return(
-    <div className='appWrapper'>
-      <div className='addTodoBox'>
-        <input 
-        type="text"
-        value = {todolistTitle}
-        onChange={(e: ChangeEvent<HTMLInputElement>) => {addTodoTitle(e.currentTarget.value)}}
+  return (
+    <div className="appWrapper">
+      <div className="addTodoBox">
+        <input
+          type="text"
+          value={todolistTitle}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => {
+            addTodoTitle(e.currentTarget.value);
+          }}
         />
-        <button onClick={() => {onClickHandler(todolistTitle)}}>add todolist</button>
+        <button
+          onClick={() => {
+            onClickHandler(todolistTitle);
+          }}
+        >
+          add todolist
+        </button>
       </div>
-      {todolistState.length > 0 ? <AddTodo list={todolistState} title={todolistTitle} err={err}/> : <span>you don't have any list</span>}
+      {todolistState.length > 0 ? (
+        <AddTodo
+          list={todolistState}
+          title={todolistTitle}
+          err={err}
+          delItem={deleteItem}
+        />
+      ) : (
+        <span>you don't have any list</span>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- src/comp-ts/inputTodo/AddTodo.tsx
+++ src/comp-ts/inputTodo/AddTodo.tsx
@@ -6,15 +6,27 @@ type AddTodoType = {
   title: string;
   list: Array<TodolistStateType>;
   err: string;
+  delItem: (id: string) => void;
 };
 
 export const AddTodo = (props: AddTodoType) => {
   return (
-    <div className={s.todolistWrapper}>
-      <button className={s.delItemBtn}>X</button>
+    <div>
       <ul>
         {props.list.map((m) => {
-          return <li>{m.title}</li>;
+          return (
+            <ul className={s.todolistWrapper}>
+              <li className={s.todolistTitle}>{m.title}</li>
+              <button
+                className={s.delItemBtn}
+                onClick={() => {
+                  props.delItem(m.id);
+                }}
+              >
+                X
+              </button>
+            </ul>
+          );
         })}
       </ul>
       {props.err.trim() !== "" ? <span>{props.err}</span> : ""}
